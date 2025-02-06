f83c20c2bc180923ac147627f9f99a262f1f933d
SankalpPyasi <sankalppyasi6@gmail.com>
2025-02-06 11:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function firstWord(s) {
-  let result = "";
-  let foundChar = false;
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== " ") {
-      result += s[i]; // Add characters to the result
-      foundChar = true;
-    } else if (foundChar) {
-      break; // Stop when the first space after a word is found
+  // your code here
+	s = s.trim();
+    let spaceIndex = s.indexOf(" ");
+    if (spaceIndex === -1) {
+        return s;
     }
-  }
-
-  return result;
+    return s.substring(0, spaceIndex);
 }
-
 // Do not change the code below
+
 const s = prompt("Enter String:");
 alert(firstWord(s));
